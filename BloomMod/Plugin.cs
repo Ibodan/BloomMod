9ec115205b21fdf015278cea1371a1066c290256
Ibodan <->
2018-10-01 21:58:58 +0900

    Add project files.

---
+++ BloomMod/Plugin.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using IllusionPlugin;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BloomMod
+{
+	public class Plugin : IPlugin
+	{
+		public string Name => "BloomMod";
+		public string Version => "1.0.0";
+
+		private Prefs prefs = new Prefs();
+		private bool prefsChangeEventRegistered = false;
+		private bool resetMode = false;
+
+		public void OnApplicationStart()
+		{
+			SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+			resetMode = prefs.NeedsReset();
+			if (resetMode) prefs.ResetCameraNames();
+		}
+
+		public void OnApplicationQuit()
+		{
+			SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+			if (resetMode) prefs.DoneReset();
+		}
+
+		private void SceneManager_sceneLoaded(Scene scene, LoadSceneMode mode)
+		{
+			if (scene.buildIndex < 2) return;
+
+			if (resetMode)
+			{
+				SharedCoroutineStarter.instance.StartCoroutine(resetEffectParamsCoroutine());
+			}
+			else
+			{
+				SharedCoroutineStarter.instance.StartCoroutine(modifyEffectParamsCoroutine());
+			}
+		}
+
+		private IEnumerator modifyEffectParamsCoroutine()
+		{
+			yield return new WaitForSecondsRealtime(0.2f);
+
+			var flags = prefs.LoadCameraNames().ToDictionary(n => n, n => false);
+
+			var timeout = Time.realtimeSinceStartup;
+			while(Time.realtimeSinceStartup - timeout < 2.0f)
+			{
+				modifyEffectParams(flags);
+				yield return new WaitForSecondsRealtime(0.2f);
+			}
+
+			if (prefsChangeEventRegistered == false)
+			{
+				prefs.ChangedEvent += prefsChanged;
+				prefsChangeEventRegistered = true;
+			}
+		}
+
+		private void modifyEffectParams(Dictionary<string,bool> flags)
+		{
+			foreach (var c in Camera.allCameras)
+			{
+				if (flags != null)
+				{
+					if (flags.ContainsKey(c.name) == false) continue;
+					if (flags[c.name] == true) continue;
+					flags[c.name] = true;
+				}
+
+				var efx = c.gameObject.GetComponent<MainEffect>();
+				if (efx == null) continue;
+				var param = ReflectionUtil.GetPrivateField<MainEffectParams>(efx, "_params");
+				if (param == null) continue;
+				param = UnityEngine.Object.Instantiate(param);
+				var modParams = prefs.LoadForCamera(c.name);
+				param.baseColorBoost = modParams.baseColorBoost;
+				param.baseColorBoostThreshold = modParams.baseColorBoostThreshold;
+				param.bloomAlphaWeightScale = modParams.bloomAlphaWeightScale;
+				param.bloomIntensity = modParams.bloomIntensity;
+				param.bloomIterations = modParams.bloomIterations;
+
+				param.textureHeight = (modParams.textureHeight <= 1.0 && c.targetTexture != null) ?
+					(int)(c.targetTexture.height * modParams.textureHeight) :
+					param.textureHeight = (int)modParams.textureHeight;
+
+				param.textureWidth = (modParams.textureWidth <= 1.0 && c.targetTexture != null) ?
+					(int)(c.targetTexture.width * modParams.textureWidth) :
+					param.textureWidth = (int)modParams.textureWidth;
+
+				ReflectionUtil.SetPrivateField(efx, "_params", param);
+				Plugin.Log("set: " + c.name);
+			}
+		}
+
+		private IEnumerator resetEffectParamsCoroutine()
+		{
+			yield return new WaitForSecondsRealtime(0.2f);
+
+			var flags = new Dictionary<string, bool>();
+
+			var timeout = Time.realtimeSinceStartup;
+			while (Time.realtimeSinceStartup - timeout < 2.0f)
+			{
+				foreach (var c in Camera.allCameras)
+				{
+					if (flags.ContainsKey(c.name)) continue;
+					var efx = c.gameObject.GetComponent<MainEffect>();
+					if (efx == null) continue;
+					var param = ReflectionUtil.GetPrivateField<MainEffectParams>(efx, "_params");
+					if (param == null) continue;
+					var resetParams = new Prefs.Params();
+					resetParams.baseColorBoost = param.baseColorBoost;
+					resetParams.baseColorBoostThreshold = param.baseColorBoostThreshold;
+					resetParams.bloomAlphaWeightScale = param.bloomAlphaWeightScale;
+					resetParams.bloomIntensity = param.bloomIntensity;
+					resetParams.bloomIterations = param.bloomIterations;
+					resetParams.textureHeight = param.textureHeight;
+					resetParams.textureWidth = param.textureWidth;
+					prefs.Reset(c.name, resetParams);
+					flags.Add(c.name, true);
+					Plugin.Log("reset: " + c.name);
+				}
+				yield return new WaitForSecondsRealtime(0.2f);
+			}
+
+			prefs.SaveCameraNames(flags.Keys.ToArray());
+		}
+
+		private void prefsChanged(Prefs sender)
+		{
+			Plugin.Log("pref changed");
+			modifyEffectParams(null);
+		}
+
+		public static void Log(string s)
+		{
+			Console.WriteLine("[BloomMod] " + s);
+		}
+
+		public void OnLevelWasLoaded(int level)
+		{
+		}
+
+		public void OnLevelWasInitialized(int level)
+		{
+		}
+
+		public void OnUpdate()
+		{
+		}
+
+		public void OnFixedUpdate()
+		{
+		}
+	}
+}
---
+++ BloomMod/Prefs.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using IllusionPlugin;
+using UnityEngine;
+
+namespace BloomMod
+{
+	class Prefs
+	{
+		public class Params
+		{
+			public float baseColorBoost = 1.2f;
+			public float baseColorBoostThreshold = 0.05f;
+			public float bloomAlphaWeightScale = 1.0f;
+			public float bloomIntensity = 0.6f;
+			public int bloomIterations = 4;
+			public float textureHeight = 1024.0f;
+			public float textureWidth = 2028.0f;
+		}
+
+		public event Action<Prefs> ChangedEvent;
+		private readonly FileSystemWatcher fileWatcher;
+
+		private static string SECTIONBASE = "BloomMod";
+
+		public Prefs()
+		{
+			fileWatcher = new FileSystemWatcher(Path.Combine(Environment.CurrentDirectory, "UserData"))
+			{
+				Filter = "modprefs.ini",
+				NotifyFilter = NotifyFilters.LastWrite,
+				EnableRaisingEvents = true
+			};
+			fileWatcher.Changed += OnFileChanged;
+		}
+
+		~Prefs()
+		{
+			fileWatcher.Changed -= OnFileChanged;
+		}
+
+		private void OnFileChanged(object sender, FileSystemEventArgs args)
+		{
+			ChangedEvent(this);
+		}
+
+		public bool NeedsReset()
+		{
+			return ModPrefs.GetBool(SECTIONBASE, "Reset", true);
+		}
+
+		public void DoneReset()
+		{
+			ModPrefs.SetBool(SECTIONBASE, "Reset", false);
+		}
+
+		public void Reset(string cameraName, Params resetParams)
+		{
+			ModPrefs.SetFloat(SECTIONBASE + "!" + cameraName, "BaseColorBoost", resetParams.baseColorBoost);
+			ModPrefs.SetFloat(SECTIONBASE + "!" + cameraName, "BaseColorBoostThreshold", resetParams.baseColorBoostThreshold);
+			ModPrefs.SetFloat(SECTIONBASE + "!" + cameraName, "BloomAlphaWeightScale", resetParams.bloomAlphaWeightScale);
+			ModPrefs.SetFloat(SECTIONBASE + "!" + cameraName, "BloomIntensity", resetParams.bloomIntensity);
+			ModPrefs.SetInt(SECTIONBASE + "!" + cameraName, "BloomIterations", resetParams.bloomIterations);
+			ModPrefs.SetFloat(SECTIONBASE + "!" + cameraName, "TextureHeight", resetParams.textureHeight);
+			ModPrefs.SetFloat(SECTIONBASE + "!" + cameraName, "TextureWidth", resetParams.textureWidth);
+		}
+
+		public string[] LoadCameraNames()
+		{
+			var names = ModPrefs.GetString(SECTIONBASE, "CameraNames");
+			return names.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries);
+		}
+
+		public void SaveCameraNames(string[] names)
+		{
+			var hash = new HashSet<string>(LoadCameraNames());
+			hash.UnionWith(names);
+			ModPrefs.SetString(SECTIONBASE, "CameraNames", string.Join(",", hash.ToArray()));
+		}
+
+		public void ResetCameraNames()
+		{
+			ModPrefs.SetString(SECTIONBASE, "CameraNames", "");
+		}
+
+		public Params LoadForCamera(string cameraName)
+		{
+			Params ret = new Params();
+			var defaults = new Params();
+
+			ret.baseColorBoost = ModPrefs.GetFloat(SECTIONBASE + "!" + cameraName, "BaseColorBoost", defaults.baseColorBoost);
+			ret.baseColorBoostThreshold = ModPrefs.GetFloat(SECTIONBASE + "!" + cameraName, "BaseColorBoostThreshold", defaults.baseColorBoostThreshold);
+			ret.bloomAlphaWeightScale = ModPrefs.GetFloat(SECTIONBASE + "!" + cameraName, "BloomAlphaWeightScale", defaults.bloomAlphaWeightScale);
+			ret.bloomIntensity = ModPrefs.GetFloat(SECTIONBASE + "!" + cameraName, "BloomIntensity", defaults.bloomIntensity);
+			ret.bloomIterations = ModPrefs.GetInt(SECTIONBASE + "!" + cameraName, "BloomIterations", defaults.bloomIterations);
+			ret.textureHeight = ModPrefs.GetFloat(SECTIONBASE + "!" + cameraName, "TextureHeight", defaults.textureHeight);
+			ret.textureWidth = ModPrefs.GetFloat(SECTIONBASE + "!" + cameraName, "TextureWidth", defaults.textureWidth);
+
+			return ret;
+		}
+	}
+}
